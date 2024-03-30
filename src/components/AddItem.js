839762dac418003723dc23c363f0b9564738ecad
Abesh <abeshsarkar2030@gmail.com>
2024-03-31 00:59:03 +0530

    keydown

--- src/components/AddItem.js
+++ src/components/AddItem.js
@@ -2,45 +2,47 @@ import React, { useRef , useContext, memo} from 'react'
 import { Context } from './../App';
 import './AddItem.css'
 
-
-// const AddItem = ({setExpnc, setTotal}) => {
 const AddItem = () => {
-    console.log("AddItems 123455");
+    const { setExpnc, setTotal } = useContext(Context);
+    const titleRef = useRef();
+    const priceRef = useRef(); 
 
-    const {setExpnc,setTotal} = useContext(Context)
+    const handleKeyDown = (event) => {
+        if (event.key === 'Enter') {
+            event.preventDefault(); // Prevents the default behavior of submitting forms
+            document.getElementById("addButton").click(); // Simulates a click on the button
+        }
+    };
 
-
-        const titleRef = useRef();
-        const priceRef = useRef(); 
-
-  return (
-    <div className='add_sec'>
-        <h1>Add Expenses</h1>
-        <div className='input_sec'>
-            <input ref={titleRef} placeholder='Enter title' type="text" />
-            <input ref={priceRef} placeholder='Enter Price' type="number" />
+    return (
+        <div className='add_sec'>
+            <h1>Add Expenses</h1>
+            <div className='input_sec'>
+                <input ref={titleRef} placeholder='Enter title' type="text" onKeyDown={handleKeyDown} />
+                <input ref={priceRef} placeholder='Enter Price' type="number" onKeyDown={handleKeyDown} />
+            </div>
+            
+            <button
+                id="addButton"
+                onClick={()=>{
+                    const title = titleRef.current.value;
+                    const price = parseInt(priceRef.current.value);
+                    if(title==="" || isNaN(price))
+                    {
+                        alert("Please add valid item");
+                    }
+                    else {
+                        setExpnc(prev=>[...prev,{title,price}]);
+                        setTotal(prevValue=>price+prevValue);
+                        titleRef.current.value="";
+                        priceRef.current.value="";   
+                    }
+                }}
+            >
+                Add
+            </button>
         </div>
-        
-        <button
-            onClick={()=>{
-                const title = titleRef.current.value;
-                const price = parseInt(priceRef.current.value);
-                if(title==="" || price==="")
-                {
-                    alert("Please add item")
-                }
-                else {
-                    setExpnc(prev=>[...prev,{title,price}])
-                    setTotal(prevValue=>price+prevValue)
-                    titleRef.current.value="";
-                    priceRef.current.value="";   
-                }
-            }}
-        >Add</button>
-    </div>
-  )
+    );
 }
 
 export default memo(AddItem);
-
- 
